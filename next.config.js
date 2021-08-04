6b30b52baa1259b9ebaa67fc3b22d9a30a4febc8
Algren Pauna <algren.pauna@gmail.com>
2021-08-04 14:22:16 +0100

    removed env var

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ module.exports = {
     env: {
         GOOGLE_CLIENT: process.env.GOOGLE_CLIENT,
         GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         DATABASE_URL: process.env.DATABASE_URL,
     }
 }
