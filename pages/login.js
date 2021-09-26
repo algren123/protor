bd383cb1e51e437e9a516ba6564acf477c6e7bc1
Algren Pauna <algren.pauna@gmail.com>
2021-09-26 14:04:47 +0100

    added www to the callback urls

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ function Login() {
             <div className="bg-dark-grey text-center w-80 h-120 rounded-md">
                 <h1 className="text-5xl text-primary-500 mt-32 font-semibold">Login</h1>
                 <h3 className="text-md text-primary-500 mt-10 font-semibold">Don't have an account? <Link href="/signup"><span className="font-bold cursor-pointer">Sign up</span></Link></h3>
-                <button className="bg-primary font-semibold rounded-full px-10 py-1 w-50 mt-3 mb-5" onClick={() => signIn("google", { callbackUrl: "https://protor.uk/"})}>Login with Google</button>
+                <button className="bg-primary font-semibold rounded-full px-10 py-1 w-50 mt-3 mb-5" onClick={() => signIn("google", { callbackUrl: "https://www.protor.uk/"})}>Login with Google</button>
             </div>
         </div>
     )
