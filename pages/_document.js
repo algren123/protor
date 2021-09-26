645ef50897df8b5b16513e6ad403ee9b7625cf0c
Algren Pauna <algren.pauna@gmail.com>
2021-09-26 12:14:31 +0100

    added AdSense

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ class MyDocument extends Document {
                 href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700;800;900&display=swap"
                 rel="stylesheet"
             />
+            <script data-ad-client="ca-pub-8348596226832523" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </Head>
         <body>
           <Main />
