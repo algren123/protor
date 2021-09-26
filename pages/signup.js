061adfeac97da5462c76532d64dea04a04e78e96
Algren Pauna <algren.pauna@gmail.com>
2021-09-26 13:45:09 +0100

    updated callback url

--- pages/signup.js
+++ pages/signup.js
@@ -7,7 +7,7 @@ function Signup() {
             <div className="bg-dark-grey text-center w-80 h-120 rounded-md">
                 <h1 className="text-5xl text-primary-500 mt-32 font-semibold">Register</h1>
                 <h3 className="text-md text-primary-500 mt-10 font-semibold">Already have an account? <Link href="/login"><span className="font-bold cursor-pointer">Login</span></Link></h3>
-                <button className="bg-primary font-semibold rounded-full px-10 py-1 w-50 mt-3 mb-5" onClick={() => signIn("google", { callbackUrl: "https://protor.vercel.app/"})}>Register with Google</button>
+                <button className="bg-primary font-semibold rounded-full px-10 py-1 w-50 mt-3 mb-5" onClick={() => signIn("google", { callbackUrl: "https://protor.uk/"})}>Register with Google</button>
             </div>
         </div>
     )
