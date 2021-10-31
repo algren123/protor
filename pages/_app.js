bfaef0244607362ab1b727c509bca9b47fe0030e
Algren Pauna <algren.pauna@gmail.com>
2021-10-31 17:14:41 +0000

    renamed stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import 'tailwindcss/tailwind.css'
-import { Provider } from 'next-auth/client'
-import './styles/radio.scss'
+import 'tailwindcss/tailwind.css';
+import { Provider } from 'next-auth/client';
+import './styles/styles.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
